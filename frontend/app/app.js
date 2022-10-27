b043dfc2700bf0378328b42031b3ca3a67a95fbf
Ilya Yudin <nibezo.cs@gmail.com>
2022-10-27 19:24:44 +0300

    fix: typo

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,6 +1,6 @@
 // JSON вернет мне {'Status': 'OK}
 
 function logout() {
-    document.cookie = "token=access_token;max-age=-1000000000000000000000000"
+    document.cookie = "name=access_token;max-age=0"
     location.reload()
 }
