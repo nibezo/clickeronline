8e95653a12b9f52183ae3daaf3a346153ca2befc
Ilya Yudin <nibezo.cs@gmail.com>
2022-10-27 19:31:16 +0300

    fix: delete max-age

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,6 +1,6 @@
 // JSON вернет мне {'Status': 'OK}
 
 function logout() {
-    document.cookie = "name=access_token;max-age=-1"
+    document.cookie = "name=access_token; expires=-1"
     location.reload()
 }
