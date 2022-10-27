ce7a6bcf235ee7a61324654876797000e9dde33b
Ilya Yudin <nibezo.cs@gmail.com>
2022-10-27 19:29:04 +0300

    fix: negative try

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,6 +1,6 @@
 // JSON вернет мне {'Status': 'OK}
 
 function logout() {
-    document.cookie = "name=access_token;max-age=-300000"
+    document.cookie = "name=access_token;max-age=-1"
     location.reload()
 }
