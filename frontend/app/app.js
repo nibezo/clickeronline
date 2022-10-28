47911a9729255ff407a1033c6254eab4313cdfd0
Ilya Yudin <nibezo.cs@gmail.com>
2022-10-28 15:06:57 +0300

    feat: add xhr for data getting

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,9 +1,22 @@
+document.addEventListener("DOMContentLoaded", scorePrepare);
+const data = 'http://127.0.0.1:8000/profile'
+
+async function getData() {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', data, true)
+    xhr.responseType = 'json'
+    xhr.onload = () => {
+        console.log(xhr.response)
+    }
+    xhr.send()
+    console.log(xhr)
+}
+
+
 async function add() {
     const response = await fetch("/click", {
-        method: "POST",
+        method: "GET",
         headers: { "Accept": "application/json", "Content-Type": "application/json" },
-        body: JSON.stringify({
-        })
     });
     console.log('OK')
 }
