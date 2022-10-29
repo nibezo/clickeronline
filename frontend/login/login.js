ab841c493152b8bbbe0ad531835d1cb5d0d29b5b
Ilya Yudin <nibezo.cs@gmail.com>
2022-10-29 14:30:49 +0300

    fix: delete commits

--- frontend/login/login.js
+++ frontend/login/login.js
@@ -5,11 +5,10 @@ function enterKeyPressed(event) {
  }
 
 async function send(){
-    // getting user's password and login
     const username = document.getElementById("username").value
     const password = document.getElementById("password").value
     
-    // отправляем запрос
+
     const response = await fetch("/signin", {
     method: "POST",
     headers: { "Accept": "application/json", "Content-Type": "application/json" },
