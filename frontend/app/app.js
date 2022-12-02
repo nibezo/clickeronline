327dc8de56a0ee4aff6f04504c2de1302a9e908c
Ilya Yudin <nibezo.cs@gmail.com>
2022-12-02 22:25:16 +0300

    feat: show leaderboard place

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,103 +1,138 @@
-const userData = '/profile'
-const getLeaderboardData = '/leaderboard'
-const buyAMeme = '/buymeme'
-const king = '/king'
-const wipe = '/wipe'
-const boost = '/boost'
-let clickCount = 0
-let userName = ''
-let colors = ['#FBF8CC', '#FDE4CF', '#FFCFD2', '#F1C0E8', '#CFBAF0', '#A3C4F3', '#8EECF5', '#98F5E1', '#B9FBC0', '#606c38', '#bde0fe', '#fca311', '#faedcd', '#e85d04', '#99d98c', '#f5cac3', '#d62828', '#b8c0ff', '#06d6a0', '#f15bb5', '#936639', '#e56b6f', '#023e7d', '#577590', '#b5179e', '#ff4d6d', '#ff4d6d', '#ff595e', '#ccff33']
+const userData = '/profile';
+const getLeaderboardData = '/leaderboard';
+const buyAMeme = '/buymeme';
+const king = '/king';
+const wipe = '/wipe';
+const boost = '/boost';
+let clickCount = 0;
+let userName = '';
+let colors = [
+  '#FBF8CC',
+  '#FDE4CF',
+  '#FFCFD2',
+  '#F1C0E8',
+  '#CFBAF0',
+  '#A3C4F3',
+  '#8EECF5',
+  '#98F5E1',
+  '#B9FBC0',
+  '#606c38',
+  '#bde0fe',
+  '#fca311',
+  '#faedcd',
+  '#e85d04',
+  '#99d98c',
+  '#f5cac3',
+  '#d62828',
+  '#b8c0ff',
+  '#06d6a0',
+  '#f15bb5',
+  '#936639',
+  '#e56b6f',
+  '#023e7d',
+  '#577590',
+  '#b5179e',
+  '#ff4d6d',
+  '#ff4d6d',
+  '#ff595e',
+  '#ccff33',
+];
 
-document.addEventListener("DOMContentLoaded", getData);
-document.addEventListener("DOMContentLoaded", getLeaderboard);
+document.addEventListener('DOMContentLoaded', getData);
+document.addEventListener('DOMContentLoaded', getLeaderboard);
 
 async function getData() {
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', userData, true)
-    xhr.responseType = 'json'
-    xhr.onload = () => {
-        console.log(xhr.response)
-        clickCount = xhr.response['money']
-        userName = xhr.response['username']
-        console.log(userName)
-        document.getElementById('score').innerHTML = `${clickCount}$`
-        document.getElementById('nickname').innerHTML = userName
-    }
-    xhr.send()
-    console.log(xhr)
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', userData, true);
+  xhr.responseType = 'json';
+  xhr.onload = () => {
+    clickCount = xhr.response['money'];
+    userName = xhr.response['username'];
+    document.getElementById('score').innerHTML = `${clickCount}$`;
+    document.getElementById('nickname').innerHTML = userName;
+  };
+  xhr.send();
 }
 
 async function add() {
-    const response = await fetch("/click", {
-        method: "GET",
-        headers: { "Accept": "application/json", "Content-Type": "application/json" },
-    });
-    console.log('OK')
-    clickCount++
-    document.getElementById('score').innerHTML = `${clickCount}$`
-    if (clickCount % 10 === 0) {
-        let color = getRandomColor()
-        let body = document.getElementsByTagName("body")
-        body[0].style.backgroundColor = color;
-    }
+  const response = await fetch('/click', {
+    method: 'GET',
+    headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+  });
+  console.log('OK');
+  clickCount++;
+  document.getElementById('score').innerHTML = `${clickCount}$`;
+  if (clickCount % 10 === 0) {
+    let color = getRandomColor();
+    let body = document.getElementsByTagName('body');
+    body[0].style.backgroundColor = color;
+  }
 }
 
 async function logout() {
-    const response = await fetch("/logout", {
-        method: "POST",
-        headers: { "Accept": "application/json", "Content-Type": "application/json" },
-        body: JSON.stringify({
-        })
-    });
-    location.reload()
+  const response = await fetch('/logout', {
+    method: 'POST',
+    headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+    body: JSON.stringify({}),
+  });
+  location.reload();
 }
 
-async function getLeaderboard () {
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', getLeaderboardData, true)
-    xhr.responseType = 'json'
-    xhr.onload = () => {
-        console.log(xhr.response['1'])
-        if (document.getElementById('gamers'))
-        for (id in xhr.response) {
-            document.getElementById('gamers').innerHTML += `
+async function getLeaderboard() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', getLeaderboardData, true);
+  xhr.responseType = 'json';
+  xhr.onload = () => {
+    if (document.getElementById('gamers')) {
+      for (id in xhr.response) {
+        document.getElementById('gamers').innerHTML += `
                 <p clsss="user">
                     <span class="id">${id}.</span>
                     <span class="username">${xhr.response[id]['username']} - </span>
                     <span class="money"><b>${xhr.response[id]['money']}$</b></span>
                 </p>
-            `
+            `;
+        if (xhr.response[id]['username'] === userName) {
+          document.getElementById('leaderplace').innerHTML = id;
         }
+      }
     }
-    xhr.send()
-    console.log(xhr)
+  };
+  xhr.send();
 }
 
 async function buyMeme() {
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', buyAMeme, true)
-    xhr.responseType = 'json'
-    xhr.onload = () => {
-        if (xhr.response['Status'] === 'OK') {
-            console.log(clickCount)
-            getData() 
-            let memeNum = getRandomIntInRange(1, 425)
-            document.getElementById('meme').innerHTML = `<img src="https://veshok.com/dw/files/Memy/%D0%9C%D0%B5%D0%BC%20(${memeNum}).jpg" width="300px" onerror="javascript: buyMeme()">`
-        }
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', buyAMeme, true);
+  xhr.responseType = 'json';
+  xhr.onload = () => {
+    if (xhr.response['Status'] === 'OK') {
+      console.log(clickCount);
+      getData();
+      let memeNum = getRandomIntInRange(1, 425);
+      document.getElementById(
+        'meme'
+      ).innerHTML = `<img src="https://veshok.com/dw/files/Memy/%D0%9C%D0%B5%D0%BC%20(${memeNum}).jpg" width="300px" onerror="javascript: buyMeme()">`;
     }
-    xhr.send()
+  };
+  xhr.send();
 }
 
 function getRandomIntInRange(from, to) {
-    let bad = [291, 315, 317, 319, 321, 323, 325, 327, 329, 331, 333, 335, 337, 339, 341, 343, 345, 347, 349, 351, 353, 355, 357, 359, 361, 363, 365, 314, 318, 322, 326, 330, 334, 338, 342, 346, 350, 354, 358, 362, 366, 320, 328, 336, 344, 352, 360, 316, 332, 348, 364, 340, 324, 356]
-    let result = Math.random()+from;
+  let bad = [
+    291, 315, 317, 319, 321, 323, 325, 327, 329, 331, 333, 335, 337, 339, 341,
+    343, 345, 347, 349, 351, 353, 355, 357, 359, 361, 363, 365, 314, 318, 322,
+    326, 330, 334, 338, 342, 346, 350, 354, 358, 362, 366, 320, 328, 336, 344,
+    352, 360, 316, 332, 348, 364, 340, 324, 356,
+  ];
+  let result = Math.random() + from;
+  result = Math.floor(Math.random() * to);
+  while (bad.includes(result)) {
     result = Math.floor(Math.random() * to);
-    while (bad.includes(result)) {
-        result = Math.floor(Math.random() * to);
-    }
-    return result;
+  }
+  return result;
 }
 
 function getRandomColor() {
-    return colors[Math.floor(Math.random() * colors.length)]
+  return colors[Math.floor(Math.random() * colors.length)];
 }
